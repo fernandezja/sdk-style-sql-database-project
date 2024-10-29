84c9d0675993c65e292be8320c0859ed7a924ef9
Jose A. Fernandez <fernandezja@gmail.com>
2024-10-29 20:59:36 -0300

    Add a table > Jedi

---
+++ src/Starwars.DB/Jedi.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Jedi] (
+    [JediId]    INT IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (100) NOT NULL,
+    [DateBirth] DATE NULL,
+    CONSTRAINT [PK_Jedi] PRIMARY KEY CLUSTERED ([JediId] ASC)
+);
